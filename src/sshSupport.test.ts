016e37b858e8d50da4e2c66da66475f7db41c37c
Kyle Carberry <kyle@carberry.com>
2023-04-17 12:57:07 -0500

    fix: check if OpenSSH supports `SetEnv`
    Fixes coder/coder#7169, coder/vscode-coder#73.

---
+++ src/sshSupport.test.ts
@@ -0,0 +1,18 @@
+import { it, expect } from "vitest"
+import { sshSupportsSetEnv, sshVersionSupportsSetEnv } from "./sshSupport"
+
+const supports = {
+  "OpenSSH_8.9p1 Ubuntu-3ubuntu0.1, OpenSSL 3.0.2 15 Mar 2022": true,
+  "OpenSSH_7.6p1 Ubuntu-4ubuntu0.7, OpenSSL 1.0.2n 7 Dec 2017": false,
+  "OpenSSH_7.4p1, OpenSSL 1.0.2k-fips  26 Jan 2017": false,
+}
+
+Object.entries(supports).forEach(([version, expected]) => {
+  it(version, () => {
+    expect(sshVersionSupportsSetEnv(version)).toBe(expected)
+  })
+})
+
+it("current shell supports ssh", () => {
+  expect(sshSupportsSetEnv()).toBeTruthy()
+})
