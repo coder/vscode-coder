01c2d80a406a3f73acd31e252c297eec95a09d84
Justin George <justin.george@gmail.com>
2025-06-16 14:41:54 -0700

    autocorrect formatting

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
-	extensionDevelopmentPath: '.',
-	extensionTestsPath: './out/test',
-	launchArgs: ['--enable-proposed-api', 'coder.coder-remote'],
+	files: "out/test/**/*.test.js",
+	extensionDevelopmentPath: ".",
+	extensionTestsPath: "./out/test",
+	launchArgs: ["--enable-proposed-api", "coder.coder-remote"],
 	mocha: {
-		ui: 'tdd',
-		timeout: 20000
-	}
+		ui: "tdd",
+		timeout: 20000,
+	},
 });
