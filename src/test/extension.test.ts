c693a460c08a67fd04820bd0bed66be15569da52
Justin George <justin.george@gmail.com>
2025-06-16 12:12:05 -0700

    enable vscode-test and bump tsconfig to modern settings

---
+++ src/test/extension.test.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+
+suite("first test", () => {
+	test("first test", () => {
+		// This is a dummy test to ensure the test suite runs
+		assert.strictEqual(1, 1);
+	});
+});
